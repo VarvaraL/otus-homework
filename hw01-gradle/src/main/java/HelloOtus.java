83c56b5c127d0f1b93abf460e7a00c715b001fe6
Vaeltai <sisterofkelt@gmail.com>
2022-07-27 13:45:06 +0300

    hw01-gradle

---
+++ hw01-gradle/src/main/java/HelloOtus.java
@@ -0,0 +1,14 @@
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+import java.util.Arrays;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+
+        String[] words = {"a", "as", "asd"};
+        Multiset<String> counts = HashMultiset.create();
+        counts.addAll(Arrays.asList(words));
+        System.out.println(counts);
+    }
+}
